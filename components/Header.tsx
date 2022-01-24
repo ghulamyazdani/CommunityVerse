55901d58b348f4dcc1c30335bf179cac19f3dafb
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-01-25 00:50:51 +0530

    prettierfrmt+sass+moresetup

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h2>Blogs</h2>
+      </div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
       </Head>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
