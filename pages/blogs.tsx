c994d1fb52e98ec31445b4ba67db872e76827a43
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-03-08 23:11:18 +0530

    blogs page added

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,5 +1,67 @@
-import React from 'react'
+import type { NextPage } from 'next'
+import '../styles/Home.module.scss'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import MainPosts from '../components/MainPosts'
+import { postInt } from '../types/interface'
+import { sortByDate } from '../utils'
+import Header from '../components/Header'
 
-export default function blogs() {
-    return <div>blogs</div>
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
+import dynamic from 'next/dynamic'
+const Rocketanim = dynamic(() => import('../components/Icons/Rocketanim'), {
+    ssr: false,
+})
+// import ToTop from '../components/use/ToTop'
+
+const Home: NextPage<any> = ({ posts }) => {
+    return (
+        <div className="dark:bg-[#0E1A38] transition-transform">
+            <Header posts={posts} />
+            <div
+                id="articles"
+                className="px-0 backdrop-opacity-10 flex flex-col items-center justify-center dark:backdrop-opacity-70 backdrop-invert dark:bg-[#000000] bg-white/30 lg:px-20"
+            >
+                <h1 className="text-center text-4xl pt-4">Read and Learn</h1>
+                <div className="posts pb-10 p-4">
+                    {posts.map((post: postInt, index: number) => {
+                        if (index <= 3) {
+                            return <MainPosts key={index} post={post} />
+                        }
+                    })}
+                </div>
+                <p className="post link--arrowed border-2 py-1 px-2 rounded-lg m-0 items-end cursor-pointer text-center text-md my-16 flex flex-row justify-center  gap-3">
+                    View All Articles
+                    <HiOutlineArrowNarrowRight className="arrow-icon w-6 h-6" />
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
+export async function getStaticProps() {
+    // Get all the files in posts dir
+    const files = fs.readdirSync(path.join('posts'))
+    // Get the content of each file
+    const posts = files.map(filename => {
+        const slug = filename.replace('.mdx', '')
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', filename),
+            'utf-8',
+        )
+        const { data: frontmatter } = matter(markdownWithMeta)
+        return {
+            slug,
+            frontmatter,
+        }
+    })
+
+    return {
+        props: {
+            posts: posts.sort(sortByDate),
+        },
+    }
 }
