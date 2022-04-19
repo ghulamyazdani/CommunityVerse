f99f5ffd57528a43243e2319687e151a661046e9
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-04-19 23:36:39 +0530

    redux setup

---
+++ state/reducers/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from '@reduxjs/toolkit'
+// import bankReducers from "./bankReducers";
+// const reducers = combineReducers({
+//   bank: bankReducers,
+// });
+// export default reducers;
+// export type State = ReturnType<typeof reducers>;
---
+++ state/store.ts
@@ -0,0 +1,5 @@
+import { applyMiddleware, createStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+// import reducers from "./reducers/index";
+
+// export const store = createStore(reducers, {}, applyMiddleware(thunk));
